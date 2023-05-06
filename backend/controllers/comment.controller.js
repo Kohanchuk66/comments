b199558628406c2ad0f0f43fd1a3def1cb26d04a
Illya <drozd.vol@gmail.com>
2023-05-06 23:04:40 +0300

    change response in comments

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -10,7 +10,6 @@ exports.getComments = async (req, res) =>{
             });
 
         return res.status(200).json({
-            count,
             comments
         });
     } catch (e) {
@@ -25,10 +24,11 @@ exports.createComment = async (req, res) =>{
         const {username, email, homePage, text} = req.body;
 
         const newComment = new Comment({username, email, homePage, text});
+        await newComment.save();
+        
         let comments = await Comment.find().lean();
         
         return res.status(201).json( {
-            count,
             comments
         });
     } catch (e) {
